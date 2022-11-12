4dfc0f1e351b64d3a82c3d04a0c22978e3c5b0ac
andre.faria <andre.faria@ceiia.com>
2022-11-12 11:56:28 +0000

    presentational component

--- reusableComponent/src/App.tsx
+++ reusableComponent/src/App.tsx
@@ -1,19 +1,34 @@
 
 import './App.css'
 import Accordion from './app/components/AccordionComponent/Accordion'
+import InputComponent from './app/components/InputComponent/InputComponent'
 // import Editable from './app/components/Editable/Editable'
 
 
 function App() {
 
+  const showValue = (value) => <><p>the value is <strong>{value}</strong></p></>
+
+
+  function multiply(value) {
+    return (
+      <>
+        <p>
+          the value is {value * 10}
+        </p>
+      </>)
+
+  }
 
   return (
     <div className="App">
       <div>
 
         <Accordion />
+        <InputComponent renderText={showValue} />
+        <br />
         {/* <Editable /> */}
-
+        <InputComponent renderText={multiply} />
       </div>
     </div>
 
