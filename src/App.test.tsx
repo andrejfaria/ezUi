cdfc9de061b4387bd5d53294a1c45119c5f763a9
andre faria <andre.faria@ceiia.com>
2023-05-29 22:49:45 +0100

    vitest

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import { describe, it } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import App from './App';
+
+describe('App', () => {
+  it('Render hello world', () => {
+    //ARRANGE
+    render(<App />)
+    //ACT
+    expect(screen.getByRole('heading', {
+      level: 1
+    })).toHaveTextContent('Hello Test World')
+    //EXPECT
+  })
+})
